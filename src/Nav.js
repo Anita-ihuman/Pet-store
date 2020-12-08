360afadb25459a57b434e424444ba383825449a5
Anita-ihuman <charlesanita403@gmail.com>
2020-12-08 04:03:34 +0100

    Set up a nav component

---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Nav.css'
+
+
+const Nav=() =>{
+    return (
+      <div className= 'nav'>
+        <p>PET WORLD</p>
+        <ul className='list'>
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>CONTACT</li>
+        </ul>
+      </div>
+    );
+}
+
+
+export default Nav
+
