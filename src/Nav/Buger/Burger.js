c4e1401e3123f477bd23a75fb64ebef35c428655
Anita-ihuman <charlesanita403@gmail.com>
2020-12-16 00:16:56 +0100

    including a login page

--- src/Nav/Buger/Burger.js
+++ src/Nav/Buger/Burger.js
@@ -1,28 +1,28 @@
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
 
-
 const Burger = (props) => {
-  return (
-    <Menu {...props}>
-      <a className="menu-item" href="./petChoice">
-        Pets
-      </a>
-      <a className="menu-item" href="/">
-        Home
-      </a>
-      <a className="menu-item" href="/login">
-        Login
-      </a>
-      <a className="menu-item" href="/about">
-        About
-      </a>
-
-      <a className="menu-item" href="/contact">
-        Contact us
-      </a>
-   
-    </Menu>
-  );
+    return (
+      
+        <Menu {...props}>
+          <a className="menu-item" href="./petChoice">
+            Pets
+          </a>
+          <a className="menu-item" href="/">
+            Home
+          </a>
+          <a className="nav-link" href={"/login"}>
+            Login
+          </a >
+          <a className="nav-link" href={"/about"}>
+          About
+          </a >
+          <a className="menu-item" href="/contact">
+            Contact us
+          </a>
+        
+        </Menu>
+     
+    );
 };
 export default Burger;
