0a5d8a650c285368aa7f96b50d4f2438f4aee2b2
Anita-ihuman <charlesanita403@gmail.com>
2020-12-15 21:14:54 +0100

    attempting to link pages

---
+++ src/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const  Footer=() =>{
+    return (
+        <div>
+            <p>created by <span>    Anita Ihuman</span></p>
+        </div>
+    )
+}
+
+
+
+export default Footer
+
--- src/Nav/Buger/Burger.js
+++ src/Nav/Buger/Burger.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
 import { NavLink } from "react-router-dom";
+import Footer from '../../Footer/Footer'
 
 const Burger = (props) => {
   return (
@@ -8,25 +9,21 @@ const Burger = (props) => {
       <a className="menu-item" href="/">
         Home
       </a>
-
+      <a className="menu-item" href="/login">
+        Login
+      </a>
       <a className="menu-item" href="/about">
         About
       </a>
 
-      <a className="menu-item" href="./">
+      <a className="menu-item" href="./services">
         Services
       </a>
 
       <a className="menu-item" href="/contact">
         Contact us
       </a>
-      <NavLink
-        className="navbar-item"
-        activeClassName="is-active"
-        to="/profile"
-      >
-        Login
-      </NavLink>
+      <Footer />
     </Menu>
   );
 };
