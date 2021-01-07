541e1e5c1517259b421b68fe9f3ea405180ee373
Anita-ihuman <charlesanita403@gmail.com>
2021-01-07 20:30:19 +0100

    added new route

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Footer from "./Footer/Footer";
 import PetChoice from "./Menu/PetChoice/petChoice";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignUp from "./Menu/Login/CreateAcount/SignUp";
-
+import SinglePet from './Menu/PetChoice/singlePet'
 // const secondPets = {
 //   img:
 //     "https://www.vettedpetcare.com/vetted-blog/wp-content/uploads/2017/08/dog-travel-certificate-shutterstock_625242014.jpg",
@@ -27,6 +27,7 @@ function App() {
           <Route exact path="/about" component={About} />
           <Route path="/login" exact component={Login} />
           <Route path="/petChoice" exact component={PetChoice} />
+          <Route path="/singlePet/:url" children={<SinglePet/>}/>
           <Route path="/contact" exact component={Contact} />
           <Route path="/signup" exact component={SignUp} />
         </Switch>
